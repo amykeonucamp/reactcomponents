6394d90716b99586702f2facc912949053cb9f32
amy <amykeophilavong@outlook.com>
2020-06-05 13:34:59 -0700

    delete Main and move all to App

--- src/App.js
+++ src/App.js
@@ -1,11 +1,40 @@
 import React from 'react';
-import logo from './logo.svg';
-import Main from './components/MainComponent';
+import { jsxElement } from "./components/JSXComponent";
+import FunctionGreeting, { ArrowFunctionGreeting } from "./components/FunctionComponent";
+import ClassGreeting from "./components/ClassComponent";
 import './App.css';
 
 function App() {
   return (
-    <Main />
+    <React.Fragment>
+        <h1>Examples of Creating React Components</h1>
+
+        {/* 
+            This JSX element is in a variable.
+        */}
+        {jsxElement}
+
+        <hr />
+
+        {/*
+            Function Component using standard function statement
+        */}
+        <FunctionGreeting />
+
+        <hr />
+
+        {/* 
+            Function Component using arrow function statement
+        */}
+        <ArrowFunctionGreeting />
+
+        <hr />
+
+        {/* 
+            Class Component inheriting React.Component
+        */}
+        <ClassGreeting />
+      </React.Fragment>
   );
 }
 
